7d7b32f6f604270cc49fac55f4a6d2929133b436
shivangigarg24 <81507874+shivangigarg24@users.noreply.github.com>
2021-04-01 13:06:29 +0530

    Add files via upload

---
+++ tools/download_1.sh
@@ -0,0 +1,14 @@
+## Copyright (c) Microsoft Corporation.
+## Licensed under the MIT license.
+
+## This code is modified by Linjie Li from Hengyuan Hu's repository.
+## https://github.com/hengyuan-hu/bottom-up-attention-vqa
+## GNU General Public License v3.0
+
+## Script for downloading data
+
+# VQA Questions
+wget https://convaisharables.blob.core.windows.net/vqa-regat/pretrained_models.zip 
+unzip pretrained_models.zip -d pretrained_models/
+rm pretrained_models.zip 
+
