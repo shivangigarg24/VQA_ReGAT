bf72ef526bc06fe0084dbbb4f51843a36684c93d
shivangigarg24 <81507874+shivangigarg24@users.noreply.github.com>
2021-04-02 15:28:44 +0530

    Update download.sh

--- tools/download.sh
+++ tools/download.sh
@@ -8,21 +8,21 @@
 ## Script for downloading data
 
 # VQA Questions
-wget -P ./data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip 
+wget -P ./data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip --no-check-certificate
 unzip ./data/v2_Questions_Train_mscoco.zip -d ./data/Questions
 rm ./data/v2_Questions_Train_mscoco.zip 
 
-wget -P ./data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip 
+wget -P ./data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip --no-check-certificate
 unzip ./data/v2_Questions_Val_mscoco.zip -d ./data/Questions
 rm ./data/v2_Questions_Val_mscoco.zip 
 
-wget -P ./data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Test_mscoco.zip 
+wget -P ./data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Test_mscoco.zip --no-check-certificate
 unzip ./data/v2_Questions_Test_mscoco.zip -d ./data/Questions
 rm ./data/v2_Questions_Test_mscoco.zip 
 
 # VQA Annotations
-wget -P ./data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip 
-wget -P ./data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip 
+wget -P ./data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip --no-check-certificate
+wget -P ./data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip --no-check-certificate
 unzip ./data/v2_Annotations_Train_mscoco.zip -d ./data/Answers
 rm ./data/v2_Annotations_Train_mscoco.zip 
 unzip ./data/v2_Annotations_Val_mscoco.zip -d ./data/Answers
@@ -30,21 +30,21 @@ rm ./data/v2_Annotations_Val_mscoco.zip
 
 # VQA cp-v2 Questions
 mkdir ./data/cp_v2_questions
-wget -P ./data/cp_v2_questions https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_train_questions.json 
-wget -P ./data/cp_v2_questions https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_test_questions.json 
+wget -P ./data/cp_v2_questions https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_train_questions.json --no-check-certificate
+wget -P ./data/cp_v2_questions https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_test_questions.json --no-check-certificate
 
 # VQA cp-v2 Annotations
 mkdir ./data/cp_v2_annotations
-wget -P ./data/cp_v2_annotations https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_train_annotations.json 
-wget -P ./data/cp_v2_annotations https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_train_annotations.json 
+wget -P ./data/cp_v2_annotations https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_train_annotations.json --no-check-certificate
+wget -P ./data/cp_v2_annotations https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_train_annotations.json --no-check-certificate
 
 # Visual Genome Annotations
 mkdir ./data/visualGenome
-wget -P ./data/visualGenome https://convaisharables.blob.core.windows.net/vqa-regat/data/visualGenome/image_data.json 
-wget -P ./data/visualGenome https://convaisharables.blob.core.windows.net/vqa-regat/data/visualGenome/question_answers.json 
+wget -P ./data/visualGenome https://convaisharables.blob.core.windows.net/vqa-regat/data/visualGenome/image_data.json --no-check-certificate
+wget -P ./data/visualGenome https://convaisharables.blob.core.windows.net/vqa-regat/data/visualGenome/question_answers.json --no-check-certificate
 
 # GloVe Vectors and dictionary
-wget -P ./data https://convaisharables.blob.core.windows.net/vqa-regat/data/glove.zip 
+wget -P ./data https://convaisharables.blob.core.windows.net/vqa-regat/data/glove.zip --no-check-certificate
 unzip ./data/glove.zip -d ./data/glove
 rm ./data/glove.zip 
 
@@ -52,30 +52,30 @@ rm ./data/glove.zip
 # adaptive
 # WARNING: This may take a while
 mkdir ./data/Bottom-up-features-adaptive
-wget -P ./data/Bottom-up-features-adaptive https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-adaptive/train.hdf5 
-wget -P ./data/Bottom-up-features-adaptive https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-adaptive/val.hdf5 
-wget -P ./data/Bottom-up-features-adaptive https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-adaptive/test2015.hdf5 
+wget -P ./data/Bottom-up-features-adaptive https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-adaptive/train.hdf5 --no-check-certificate
+wget -P ./data/Bottom-up-features-adaptive https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-adaptive/val.hdf5 --no-check-certificate
+wget -P ./data/Bottom-up-features-adaptive https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-adaptive/test2015.hdf5 --no-check-certificate
 
 # fixed
 # WARNING: This may take a while
 mkdir ./data/Bottom-up-features-fixed
-wget -P ./data/Bottom-up-features-fixed https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-fixed/train36.hdf5 
-wget -P ./data/Bottom-up-features-fixed https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-fixed/val36.hdf5 
-wget -P ./data/Bottom-up-features-fixed https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-fixed/test2015_36.hdf5 
+wget -P ./data/Bottom-up-features-fixed https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-fixed/train36.hdf5 --no-check-certificate
+wget -P ./data/Bottom-up-features-fixed https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-fixed/val36.hdf5 --no-check-certificate
+wget -P ./data/Bottom-up-features-fixed https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-fixed/test2015_36.hdf5 --no-check-certificate
 
 # imgids
-wget -P ./data/ https://convaisharables.blob.core.windows.net/vqa-regat/data/imgids.zip 
+wget -P ./data/ https://convaisharables.blob.core.windows.net/vqa-regat/data/imgids.zip --no-check-certificate
 unzip ./data/imgids.zip -d ./data/imgids
 rm ./data/imgids.zip 
 
 # Download Pickle caches for the pretrained model
 # and extract pkl files under data/cache/.
-wget -P ./data https://convaisharables.blob.core.windows.net/vqa-regat/data/cache.zip 
+wget -P ./data https://convaisharables.blob.core.windows.net/vqa-regat/data/cache.zip --no-check-certificate
 unzip ./data/cache.zip -d ./data/cache
 rm ./data/cache.zip 
 
 # Download pretrained models
 # and extract files under pretrained_models.
-wget -P ./pretrained_models https://convaisharables.blob.core.windows.net/vqa-regat/pretrained_models.zip 
+wget -P ./pretrained_models https://convaisharables.blob.core.windows.net/vqa-regat/pretrained_models.zip --no-check-certificate
 unzip ./pretrained_models/pretrained_models.zip -d ./pretrained_models
 rm ./pretrained_models/pretrained_models.zip
